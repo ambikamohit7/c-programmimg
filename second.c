e2b74a95d2f397ec3b6f1b215fa182963c87b5ce
ambikamohit7 <155753800+ambikamohit7@users.noreply.github.com>
2024-01-05 21:14:38 +0545

    Add files via upload

---
+++ second.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    printf("i am curently studying in farwestern university\n");
+    printf("i have passed my +2 from radiant higher secondary school\n");
+    return 0;
+}
