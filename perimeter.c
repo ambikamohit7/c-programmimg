29bbf123b1e01fbea25d02acce713c726b8b02c3
ambikamohit7 <155753800+ambikamohit7@users.noreply.github.com>
2024-01-07 13:39:17 +0545

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int a,b,p;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
+
+    p=2(a+b);
+    printf("the perimeter is :-%d", p);
+    return 0;
+}
