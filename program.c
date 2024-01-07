d7aee55929bd24c19483cc2c94dc03613b4db9f9
ambikamohit7 <155753800+ambikamohit7@users.noreply.github.com>
2024-01-07 13:49:05 +0545

    Add files via upload

---
+++ program.c
@@ -0,0 +1,15 @@
+// this is the program for division.
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int a=8, b=4, A;
+    printf("enter a");
+    scanf("%d", &a);
+
+    printf("enter b");
+    scanf("%d", &b);
+
+    A=a/b;
+    printf("the value is :- %d", A);
+    return 0;
+}
