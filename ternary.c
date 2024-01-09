c7aa626c3bf832715cb07e74947f52d62fc2aa78
ambikamohit7 <155753800+ambikamohit7@users.noreply.github.com>
2024-01-09 11:50:53 +0545

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,11 @@
+// this program is for ternary operator.
+#include <stdio.h>
+int main()
+{
+    system("cls");
+
+    int age = 16;
+    printf("You are %s \n ", (age >= 18) ? "Adult" : "Not Adult");
+    // in avove 8th line we have use the ternary operator.
+    return 0;
+}
