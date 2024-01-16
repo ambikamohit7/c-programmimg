dd5a0301c8cfb6509f78d822c57bbd8b7bc71723
ambikamohit7 <155753800+ambikamohit7@users.noreply.github.com>
2024-01-16 21:05:58 +0545

    Add files via upload

---
+++ conditionalswitch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    int day;
+    printf("enter the day (1-7)");
+    scanf("%d", &day);
+
+    switch (day)
+    {
+    case 1:
+        printf("sunday\n");
+        break;
+    case 2:
+        printf("monday\n");
+        break;
+    case 3:
+        printf("tuesday\n");
+        break;
+    case 4:
+        printf("wednesday\n");
+        break;
+    case 5:
+        printf("thrusday\n");
+        break;
+    case 6:
+        printf("friday\n");
+        break;
+    case 7:
+        printf("saturday\n");
+        break;
+
+    default: printf("not the valid day\n");
+        break;
+    }
+}
